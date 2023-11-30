ff65b0700a1f03cd81ca65936139b1553f757ba7
Kristen <iamkristen220@gmail.com>
2023-11-30 15:33:19 +0000

    changes

--- model/footballModel.js
+++ model/footballModel.js
@@ -2,12 +2,12 @@ const {model,Schema} = require("mongoose");
 
 const footballSchema = new Schema({
     Team :{ type: String, required:true},
-    GamesPlayed: {type: Number, required: true},
+    "Games Played": {type: Number, required: true},
     Win: {type: Number, required: true},
     Draw: {type: Number, required: true},
     Loss: {type: Number, required: true},
-    GoalsFor: {type: Number, required: true},
-    GoalsAgainst: {type: Number, required: true},
+    "Goals For": {type: Number, required: true},
+    "Goals Against": {type: Number, required: true},
     Points: {type: Number, required: true},
     Year: {type: Number, required: true}
 });
